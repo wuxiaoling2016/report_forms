c780732c0a4365e4a9b8501746902791fd286c1c
Jun <Jun@192.168.67.1>
2017-05-25 22:23:56 +0800

    完成了用例图，思维导图，数据建模和User实体类

---
+++ src/com/jxc/model/User.java
@@ -0,0 +1,29 @@
+package com.jxc.model;
+
+import java.io.Serializable;
+
+public class User implements Serializable{
+	private static final long serialVersionUID = 1L;
+	private Integer uid;
+	private String uname;
+	private String upwd;
+	public Integer getUid() {
+		return uid;
+	}
+	public void setUid(Integer uid) {
+		this.uid = uid;
+	}
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	public String getUpwd() {
+		return upwd;
+	}
+	public void setUpwd(String upwd) {
+		this.upwd = upwd;
+	}
+	
+}
